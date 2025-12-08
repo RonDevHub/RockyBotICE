3a1dd600e73e9631fc0de9c35ed6b3b1f901a56a
RonDevHub <no_reply@rondev.de>
2025-12-08 10:55:20 +0000

    public/_modals.php hinzugefügt

---
+++ public/_modals.php
@@ -0,0 +1,37 @@
+<!-- Modal -->
+    <div id="donateModal" class="modal">
+        <div class="modal-content">
+            <span class="close" data-close>&times;</span>
+            <h2>
+                ☕️  Unterstützen
+            </h2>
+            <p>Wenn dir <strong><?php echo htmlspecialchars($config['botname']); ?></strong> gefällt, kannst du mich gern auf einen virtuellen Kaffee einladen ☕️</p>
+            <span class="donate"><strong>PayPal:</strong> <a href="https://www.paypal.com/paypalme/Depressionist1/4,99" rel="noopener noreferrer" target="_blank">paypal.me/Depressionist1</a></span>
+            <span class="donate"><strong>Ko-fi:</strong> <a href="https://ko-fi.com/U6U31EV2VS" rel="noopener noreferrer" target="_blank">ko-fi.com/rondev</a></span>
+            <span class="donate"><strong>Buymeacoffee:</strong> <a href="https://www.buymeacoffee.com/RonDev" rel="noopener noreferrer" target="_blank">buymeacoffee.com/RonDev</a></span>
+            <span class="donate"><strong>Github:</strong> <a href="https://github.com/sponsors/RonDevHub" rel="noopener noreferrer" target="_blank">github.com/sponsors/RonDevHub</a></span>
+        </div>
+    </div>
+    <div id="infoModal" class="modal">
+        <div class="modal-content">
+            <span class="close" data-close>&times;</span>
+            <h2>Infos</h2>
+            <p>Vielen Dank an <a href="https://dreimetadaten.de" rel="noopener noreferrer" target="_blank">dreimetadaten.de</a> für die Bereitstellung der Metadaten.</p>
+            <p>Diese Seite wird nicht von Europa, Kosmos oder anderen Unternehmen/Personen, die in die Produktion der Serie Die drei ??? eingebunden sind, betrieben.</p>
+            <p>Alle geschützten Marken, Bilder und Texte gehören ihren jeweiligen Besitzern.</p>
+            <p><center><a href="https://commitcloud.net/RonDevHub/RockyBotICE" rel="noopener noreferrer" target="_blank"><img src="https://mini-badges.rondevhub.de/forgejo/RonDevHub/RockyBotICE/release/*/*/de"></a></center></p>
+        </div>
+    </div>
+    <div id="contactModal" class="modal">
+        <div class="modal-content">
+            <span class="close" data-close>&times;</span>
+            <h2>Kontakt</h2>
+            <p>Du möchtest gern mit mir in Kontakt treten, dann hast du die folgenden Möglichkeiten. <i>Zum Schutz vor Email-Lavinen, wird diese durch Mailhide.io geschützt</i></p>
+            <p>
+              E-MAIL: <a href="https://mailhide.io/e/Z77Evb5z" onclick="popup=window.open('https://mailhide.io/de/e/Z77Evb5z','mailhidepopup','width=580,height=635'); return false;">klicken@dannMail</a><br>
+              Martix: <a href="https://matrix.to/#/@rondev:matrix.s3cr.net" rel="noopener noreferrer" target="_blank">@rondev:matrix.s3cr.net</a> <i>Privatchat</i><br>
+              Martix: <a href="https://matrix.to/#/#RockyBotICE:matrix.s3cr.net" rel="noopener noreferrer" target="_blank">#RockyBotICE:matrix.s3cr.net</a> <i>Öffentlicher Chat</i><br>
+              GitHub: <a href="https://github.com/RonDevHub/RockyBotICE/issues" rel="noopener noreferrer" target="_blank">Issues</a>
+            </p>
+        </div>
+    </div>
